acee3e1827bcc78ebf3576b2e7bc1b67ff3223e0
Erik Barroso <coderik1996@gmail.com>
2024-06-25 10:21:48 -0300

    Lógica customizada : BEFORE CREATE

--- srv/main/routes/purchase-order-management.ts
+++ srv/main/routes/purchase-order-management.ts
@@ -1,4 +1,6 @@
+import '../configuration/module-alias';
 import { db } from "@/common/entities/db/models";
+import { service } from '@sap/cds';
 import {  Service, Request } from "@sap/cds/apis/services";
 
 //alterando o comportamento de leitura: alterando texto do cabeçalho
@@ -6,6 +8,26 @@ import {  Service, Request } from "@sap/cds/apis/services";
 export default (service: Service) => {
   service.after('READ', 'PurchaseOrderHeaders', (results: db.models.PurchaseOrderHeaders[], request: Request) => {
     results.forEach(poHeader => poHeader.company = 'XXXXXXXXXXX' )
+  })
+
+  service.before('CREATE','PurchaseOrderHeaders', (request: Request) => {
+    const items = [
+      {
+        material: 'Teste_erik',
+        quantity: 10,
+        uom: 'PC',
+        netPrice: 30,
+        center: 'ABCD'
+      },
+      {
+        material: 'Teste_erik2',
+        quantity: 5,
+        uom: 'KG',
+        netPrice: 500,
+        center: 'ABCD'
+      }
+    ]
+    request.data.items = items
   })
 }
 
